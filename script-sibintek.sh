dad6e42c19c2c606429c497ee00f04cf8886daa2
bandishoot <134743388+bandishoot@users.noreply.github.com>
2023-09-26 12:00:36 +0300

    Update script-sibintek.sh

--- script-sibintek.sh
+++ script-sibintek.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-mkdir code
-dir=./code
-URL=https://github.com/Lissy93/dashy.git
+DEST_DIR="./code"
+REPO_URL="https://github.com/Lissy93/dashy.git"
+C_NAME="dashy-app"
 
-git clone $URL $dir
+git clone $REPO_URL $DEST_DIR
 
-sudo docker build -t dashy-image:latest ./code
+cd $DEST_DIR
+sudo docker build -t dashy-local:latest . -f ../Dockerfile-local
 
-sudo docker run -d --name dashy-app dashy-image:latest
+sudo docker stop ${C_NAME}
+sudo docker rm ${C_NAME}
+sudo docker run -d --name ${C_NAME} -p 4000:80 --restart=always dashy-local:latest
 
-
-
-
-
+# simple check
+resp_code=$(curl -Is 127.0.0.1:4000 | awk 'NR==1 {print $2}')
+if [ $resp_code -eq "200" ]; then
+  echo "Script finished success"
+else
+  exit 1
+fi
